a17ad84a79bf06cec777cf2e29b9db4e3d819449
adityasunke2004 <adityasunke2004@vt.edu>
2025-06-03 13:04:51 -0400

    Updated the aes.py file
    Added a skeleton for implementing the encryption process

--- aes.py
+++ aes.py
@@ -1,2 +1,25 @@
 # class AES:
 # Implements AES encryption and decryption from scratch.
+
+class Encryption:
+    def __init__(self, plaintext):
+        self.paintext = plaintext
+        self.sblock = None # Define S-Block and permute values
+        self.predefined = None # Used in the mix_columns function
+        self.roundkey = None # Used in the round_key function
+
+    # Return State Array
+    def sub_bytes(self):
+        pass
+
+    # Return shifted state array
+    def shift_rows(self):
+        pass
+
+    # Return new state array
+    def mix_columns(self):
+        pass
+
+    # Return Final State Array
+    def round_key(self):
+        pass
