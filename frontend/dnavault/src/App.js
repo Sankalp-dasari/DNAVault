f6cff7ff1dad4739388635fe46b1de170755cec1
Ishaannjain <108075974+Ishaannjain@users.noreply.github.com>
2025-06-07 19:28:52 +0530

    started frontend

---
+++ frontend/dnavault/src/App.js
@@ -0,0 +1,11 @@
+import "./css/style.css";
+
+function App() {
+  return (
+    <div>
+      <video src="/video.mp4" loop autoPlay muted playsInline></video>
+    </div>
+  );
+}
+
+export default App;
